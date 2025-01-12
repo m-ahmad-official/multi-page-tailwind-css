00d29db0725e3865e31587d06e8aaaf554830313
Muhammad Ahmad <aq320646@gmail.com>
2025-01-12 05:20:01 +0500

    Milestone 2

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,4 +1,4 @@
-export default function Home() {
+export default function About() {
     return (
         <h1 className="h-screen bg-[#13293D] text-white text-3xl font-bold flex justify-center items-center">
             About Page.
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,4 +1,4 @@
-export default function Home() {
+export default function Contact() {
     return (
         <h1 className="h-screen bg-[#13293D] text-white text-3xl font-bold flex justify-center items-center">
             Contact Page.
